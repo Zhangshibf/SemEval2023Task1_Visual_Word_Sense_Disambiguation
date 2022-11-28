54f9f820e6793ea436d71b9695ba779118acb4ea
zhangshibf <zhangshibf@gmail.com>
2022-11-28 15:46:18 +0100

    a

--- model.py
+++ model.py
@@ -3,7 +3,26 @@
 import torch
 from PIL import Image
 import requests
-from transformers import CLIPProcessor, CLIPModel
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel
 
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+class lv_model():
+    def __int__(self):
+        super(lv_model, self).__init__()
+        self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.image_encoder = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+
+#        self.clip= CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+        self.linear = nn.Linear(,out_features = 10)
+        self.softmax = nn.Softmax()
+    def forward(self,image,text):
+        pass
+
+
+
+
+
+
+
+model =
