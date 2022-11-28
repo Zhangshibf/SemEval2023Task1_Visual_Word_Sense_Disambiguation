be5922592f54910adb82a7c0f4955d96a5b83477
zhangshibf <zhangshibf@gmail.com>
2022-11-28 15:07:21 +0100

    a

--- model.py
+++ model.py
@@ -1,3 +1,9 @@
 #contrastive learning
 #models: ViLT, FLAVA, CLIP, data2vec
 import torch
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPModel
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
