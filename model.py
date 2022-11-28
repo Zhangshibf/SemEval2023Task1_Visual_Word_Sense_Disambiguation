bb5ffc3107b2999a113f2b57a2fad5574da32c5b
zhangshibf <zhangshibf@gmail.com>
2022-11-28 16:13:01 +0100

    a

--- model.py
+++ model.py
@@ -3,26 +3,29 @@
 import torch
 from PIL import Image
 import requests
-from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel,CLIPVisionModel
+from torch import nn
 
-class lv_model():
+class clip_model():
     def __int__(self):
-        super(lv_model, self).__init__()
+        super(clip_model, self).__init__()
         self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
         self.image_encoder = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-        self.tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-
-#        self.clip= CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.linear = nn.Linear(,out_features = 10)
+        self.linear = nn.Linear(600,out_features = 2)
         self.softmax = nn.Softmax()
     def forward(self,image,text):
-        pass
+        #encode text
+        text_outputs = self.text_encoder(text)
+        text_emd1 = text_outputs.last_hidden_state
+        text_emd2 = text_outputs.pooler_output
 
+        #encode image
+        image_outputs = self.image_encoder(image)
+        image_emd1 = image_outputs.last_hidden_state
+        image_emd2 = image_outputs.pooler_output
 
+        concatenation = "some way to concatenate text embeddings and image embeddings"
+        output_linear = self.linear(concatenation)
+        prediction = self.softmax(output_linear)
 
-
-
-
-
-model =
+        return prediction
