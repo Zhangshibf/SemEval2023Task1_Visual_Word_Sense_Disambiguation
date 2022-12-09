f9999b3b2099d21129dd0e7cb2c3e3d397bca731
zhangshibf <zhangshibf@gmail.com>
2022-12-09 14:44:28 +0100

    a

---
+++ check_context.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import spacy
+import torch
+import matplotlib.pyplot as plt
+from transformers import BertTokenizer, BertModel
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+a = pd.read_csv("train.data.v1.txt",sep = "\t",header=None)
+keywords = list(a[0])
+phrases = list(a[1])
+
+# Load the BERT model and tokenizer
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased")
+
+# Generate the word embeddings for each phrase using BERT
+embeddings = []
+for phrase in phrases:
+    input_ids = tokenizer.encode(phrase, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(input_ids)
+        last_hidden_states = outputs[0]
+    embeddings.append(last_hidden_states[0][0].numpy())
+
+
+# Use elbow method to determine optimal number of clusters
+sse = []
+for k in range(3, 20):
+    kmeans = KMeans(n_clusters=k, random_state=1)
+    kmeans.fit(embeddings)
+    sse.append(kmeans.inertia_)
+
+# Plot the results to visualize the elbow
+plt.plot(range(2, 11), sse)
+plt.xlabel("Number of clusters")
+plt.ylabel("SSE")
+plt.show()
+
+"""
+# Use the optimal number of clusters to fit the k-means model
+optimal_k = # determined using elbow method
+kmeans = KMeans(n_clusters=optimal_k, random_state=1)
+kmeans.fit(embeddings)
+
+# Generate clusters for each phrase
+clusters = kmeans.predict(embeddings)
+
+# Use silhouette analysis to evaluate the quality of the clusters
+silhouette_avg = silhouette_score(embeddings, clusters)
+print("The average silhouette score is:", silhouette_avg)"""
