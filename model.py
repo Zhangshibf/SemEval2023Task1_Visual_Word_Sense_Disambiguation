4aedb5a7eb8278a499d82df163e1e8b3904eebbe
zhangshibf <zhangshibf@gmail.com>
2022-12-20 11:05:24 +0100

    a

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 #contrastive learning
-#models: ViLT, FLAVA, CLIP, data2vec
+#models: ViLT, FLAVA, CLIP
+import os
 import torch
+from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import requests
 from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel,CLIPVisionModel
@@ -29,3 +31,52 @@ class clip_model():
         prediction = self.softmax(output_linear)
 
         return prediction
+
+
+
+class ImageTextDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.image_filenames = []
+        self.texts = []
+
+        # Load the image filenames and texts into the lists
+        for file in os.listdir(data_dir):
+            if file.endswith(".jpg"):
+                self.image_filenames.append(os.path.join(data_dir, file))
+            elif file.endswith(".txt"):
+                with open(os.path.join(data_dir, file), "r") as f:
+                    self.texts.append(f.read())
+
+    def __len__(self):
+        return len(self.image_filenames)
+
+    def __getitem__(self, idx):
+        # Load the image and text
+        image = Image.open(self.image_filenames[idx])
+        text = self.texts[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, text
+
+# Example usage:
+
+# Create a transform to preprocess the images
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# Create the dataset
+dataset = ImageTextDataset("/path/to/data", transform=transform)
+
+# Create the dataloader
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True, num_workers=4)
+
+# Iterate through the dataloader
+for images, texts in dataloader:
+    # Use the images and texts to train your model
+    model.train(images, texts)
