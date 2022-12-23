92a63b9dd6d1bc9e9c259fc8e7410c5f8056b4fd
zhangshibf <zhangshibf@gmail.com>
2022-12-23 11:12:57 +0100

    a

---
+++ load_data.py
@@ -0,0 +1,53 @@
+import os
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import torchvision.transforms as transforms
+import pandas as pd
+
+
+class ImageTextDataset(Dataset):
+    def __init__(self, data_dir, data_type,
+                 transform=transforms.Compose(
+                     [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                      ])):
+        types = ["inaturalist", "train"]
+        if data_type not in types:
+            raise ValueError("Invalid data type. Expected one of: %s" % data_type)
+        self.data_dir = data_dir
+        self.transform = transform
+        self.image_path = list()
+
+        if data_type == "inaturalist":
+            # I will write this part later
+            pass
+        elif data_type == "train":
+            # this is for the original train set of the task
+            # train.data.v1.txt, train.gold.v1.txt
+            train_data = pd.read_csv(os.path.join(data_dir, "train.data.v1.txt"), sep="\t", header=None)
+            label_data = pd.read_csv(os.path.join(data_dir, "train.gold.v1.txt"), sep="\t", header=None)
+            keywords = list(train_data[0])
+            contexts = list(train_data[1])
+
+            self.keywords = keywords
+            self.text = contexts
+            image_filenames = list(label_data[0])
+            for filename in image_filenames:
+                self.image_path.append(os.path.join(data_dir, "train_images_v1", filename))
+
+    def __len__(self):
+        return len(self.text)
+
+    def __getitem__(self, idx):
+        # Load the image and text
+        image = Image.open(self.image_path[idx])
+        text = self.texts[idx]
+        keyword = self.keywords[idx]
+        if self.transform:
+            image = self.transform(image)
+
+        return keyword,text,image
+
+# Create the dataset
+dataset = ImageTextDataset(path_train, data_type="train")
+# Create the dataloader
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
