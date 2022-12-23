e888f98238f321c50d590e36963cdd56a395bf67
zhangshibf <zhangshibf@gmail.com>
2022-12-23 11:18:19 +0100

    a

--- load_data.py
+++ load_data.py
@@ -3,7 +3,7 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import torchvision.transforms as transforms
 import pandas as pd
-
+import pickle
 
 class ImageTextDataset(Dataset):
     def __init__(self, data_dir, data_type,
@@ -23,8 +23,8 @@ class ImageTextDataset(Dataset):
         elif data_type == "train":
             # this is for the original train set of the task
             # train.data.v1.txt, train.gold.v1.txt
-            train_data = pd.read_csv(os.path.join(data_dir, "train.data.v1.txt"), sep="\t", header=None)
-            label_data = pd.read_csv(os.path.join(data_dir, "train.gold.v1.txt"), sep="\t", header=None)
+            train_data = pd.read_csv(os.path.join(data_dir, "trial.data.v1.txt"), sep="\t", header=None)
+            label_data = pd.read_csv(os.path.join(data_dir, "trial.gold.v1.txt"), sep="\t", header=None)
             keywords = list(train_data[0])
             contexts = list(train_data[1])
 
@@ -47,7 +47,11 @@ class ImageTextDataset(Dataset):
 
         return keyword,text,image
 
-# Create the dataset
-dataset = ImageTextDataset(path_train, data_type="train")
-# Create the dataloader
-dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+if __name__ == "__main__":
+    # Create the dataset
+    dataset = ImageTextDataset("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1", data_type="train")
+    # Create the dataloader
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    with open("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1/dataloader.pk", 'wb') as f:
+        pickle.dump(dataloader, f)
+
