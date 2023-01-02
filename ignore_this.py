25c8ce7555a86e0c02927fd73e9a9b7536df228e
zhangshibf <zhangshibf@gmail.com>
2023-01-02 15:07:18 +0100

    a

--- ignore_this.py
+++ ignore_this.py
@@ -1,16 +1,45 @@
 from PIL import Image
 import requests
+import os
+import torch
+from load_data import *
+from torch.utils.data import Dataset, DataLoader,random_split
+import requests
+import torchvision.transforms as transforms
+from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel,CLIPVisionModel
+from torch import nn
+import pandas as pd
+from nltk.corpus import wordnet as wn
+import nltk
+from sentence_transformers import SentenceTransformer, util
+import torch
+from math import log
+from torch import optim
 from transformers import CLIPProcessor, CLIPVisionModel
 
 model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+#image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(images=image, return_tensors="pt")
+#inputs = processor(images=image, return_tensors="pt")
+image_paths = []
 
-outputs = model(**inputs)
-last_hidden_state = outputs.last_hidden_state
-pooled_output = outputs.pooler_output  # pooled CLS states
-print(pooled_output)
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+transform = transforms.Compose(
+    [transforms.Resize([1440, 1810]), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+     ])
+for path in image_paths:
+    image = Image.open(path)
+    if image.mode != "RGB":
+        image = image.convert('RGB')
+    image = transform(image)
+#        image = image.unsqueeze(0)
+    image = processor(images=image, return_tensors="pt")
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    last_hidden_state = outputs.last_hidden_state
+    pooled_output = outputs.pooler_output  # pooled CLS states
+
+    print(pooled_output.shape())
