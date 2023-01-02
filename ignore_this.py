07206dd44cc9028e5fc62affd0b91ec897b98a86
zhangshibf <zhangshibf@gmail.com>
2023-01-02 11:16:10 +0100

    a

---
+++ ignore_this.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import requests
+from transformers import CLIPProcessor, CLIPVisionModel
+
+model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+
+inputs = processor(images=image, return_tensors="pt")
+
+outputs = model(**inputs)
+last_hidden_state = outputs.last_hidden_state
+pooled_output = outputs.pooler_output  # pooled CLS states
+print(pooled_output)
