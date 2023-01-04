6f865bdf448df116a6509dcbd6ecfd424e01e24c
zhangshibf <zhangshibf@gmail.com>
2023-01-04 16:14:44 +0100

    a

--- try_model.py
+++ try_model.py
@@ -78,6 +78,7 @@ def main():
   print("\nBegin contrastive loss demo \n")
 
   loss_func = ContrastiveLoss()
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
   y1 = T.tensor([[1.0, 2.0, 3.0],
                  [3.0, 4.0, 5.0]], dtype=T.float32).to(device)
