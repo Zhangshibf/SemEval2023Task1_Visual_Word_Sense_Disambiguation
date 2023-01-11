983589fa2fc7bcb6dc156a181668e0d15f74ed65
zhangshibf <zhangshibf@gmail.com>
2023-01-11 10:17:04 +0100

    a

--- check_context.py
+++ check_context.py
@@ -1,54 +1,63 @@
-import pandas as pd
-import spacy
+from load_data import *
+from PIL import Image
+import argparse
+from PIL import ImageFile
+import torchvision.transforms as transforms
+from torch import nn
+from transformers import CLIPTextConfig,CLIPProcessor, CLIPVisionModelWithProjection,CLIPTokenizer, CLIPTextModelWithProjection
 import torch
-import matplotlib.pyplot as plt
-from transformers import BertTokenizer, BertModel
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
-a = pd.read_csv("/home/CE/zhangshi/SemEval23/train.data.v1.txt",sep = "\t",header=None)
-keywords = list(a[0])
-phrases = list(a[1])
+from torch import optim
+class clip_model(nn.Module):
+    def __init__(self):
+        super(clip_model, self).__init__()
+        self.text_encoder = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
+        self.image_encoder = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
 
-# Load the BERT model and tokenizer
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = BertModel.from_pretrained("bert-base-uncased")
+    def forward(self, text, image,setting):
+        if setting == "text":
+            text_outputs = self.text_encoder(text)
+            return text_outputs
 
-# Generate the word embeddings for each phrase using BERT
-embeddings = []
-for phrase in phrases:
-    input_ids = tokenizer.encode(phrase, return_tensors="pt")
-    with torch.no_grad():
-        outputs = model(input_ids)
-        last_hidden_states = outputs[0]
-    embeddings.append(last_hidden_states[0][0].numpy())
+        elif setting == "image":
+            image_outputs = self.image_encoder(image)
+            return image_outputs
+        else:
+            text_outputs = self.text_encoder(text)
+            image_outputs = self.image_encoder(image)
+            return text_outputs,image_outputs
+def open_images(image_paths):
+    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    transform = transforms.Compose(
+        [transforms.Resize([1440, 1810]), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+         ])
+    images = list()
+    for path in image_paths:
+        image = Image.open(path)
+        if image.mode != "RGB":
+            image = image.convert('RGB')
+        image = transform(image)
+        image = processor(images=image, return_tensors="pt")
+        images.append(image)
 
+    return images
+model = clip_model()
 
-# Use elbow method to determine optimal number of clusters
-sse = []
-for k in range(19, 50):
-    kmeans = KMeans(n_clusters=k, random_state=1)
-    kmeans.fit(embeddings)
-    sse.append(kmeans.inertia_)
+text = "I had an ice cream today. It was nice!"
+paths = ["/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/train_v1/train_images_v1/image.6331.jpg","/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/train_v1/train_images_v1/image.6331.jpg"]
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32",model_max_length=77)
+tokens = torch.tensor([tokenizer.encode(text,max_length=77,truncation=True)])
+text_emds = model(tokens,None,setting = "text")
 
-for a,b in enumerate(sse):
-    print(a)
-    print(b)
-    print("----------")
-# Plot the results to visualize the elbow
-plt.plot(range(19, 50), sse)
-plt.xlabel("Number of clusters")
-plt.ylabel("SSE")
-plt.show()
+images = open_images(paths)
+image_emds = list()
+for k in images:
+    input_image = k['pixel_values']
+    input_image = input_image.to(device)
+    outputs = model(None, input_image, setting="image")
+    image_emds.append(outputs.image_embeds)
 
-"""
-# Use the optimal number of clusters to fit the k-means model
-optimal_k = # determined using elbow method
-kmeans = KMeans(n_clusters=optimal_k, random_state=1)
-kmeans.fit(embeddings)
+text_emds_p, image_emds_p = model(tokens, images[0], setting="balbla")
 
-# Generate clusters for each phrase
-clusters = kmeans.predict(embeddings)
-
-# Use silhouette analysis to evaluate the quality of the clusters
-silhouette_avg = silhouette_score(embeddings, clusters)
-print("The average silhouette score is:", silhouette_avg)"""
+print(text_emds)
+print(text_emds_p)
