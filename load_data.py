a6a2c1800fb75d69c90a45aebfabc53b7bb1632c
zhangshibf <zhangshibf@gmail.com>
2023-01-12 14:20:33 +0100

    a

--- load_data.py
+++ load_data.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
     parser.add_argument("--output",help = "path to save the dataloader")
     args = parser.parse_args()
 
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
     # Create the dataset
     dataset = ImageTextDataset(args.train, data_type="train",device = device, text_augmentation=True)
     # Split the dataloader into train, dev, and test sets
