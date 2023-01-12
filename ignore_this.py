8aa10c554bba3c0312a6d926d497a3bfdf31309d
zhangshibf <zhangshibf@gmail.com>
2023-01-12 13:27:49 +0100

    a

--- ignore_this.py
+++ ignore_this.py
@@ -1,104 +1,35 @@
-from PIL import Image
-import requests
-import os
-import torch
+#fine tune CLIP model
 from load_data import *
-from torch.utils.data import Dataset, DataLoader,random_split
-import requests
-import torchvision.transforms as transforms
-from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer, CLIPTextModel,CLIPVisionModel
-from torch import nn
-import pandas as pd
-
-import torch
 from PIL import Image
-import requests
-from transformers import CLIPProcessor, CLIPVisionModel
+import argparse
+from PIL import ImageFile
+import torchvision.transforms as transforms
+from torch import nn
+from transformers import CLIPProcessor, CLIPVisionModelWithProjection,CLIPTokenizer, CLIPTextModelWithProjection
+import torch
+from torch import optim
+import clip
 
-model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
-
-inputs = processor(images=image, return_tensors="pt")
-print(type(inputs))
-print(inputs)
-print(type(**inputs))
-outputs = model(**inputs)
-last_hidden_state = outputs.last_hidden_state
-pooled_output = outputs.pooler_output  # pooled CLS states
-
-"""
 class clip_model(nn.Module):
     def __init__(self):
         super(clip_model, self).__init__()
-        self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.image_encoder = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
-    def forward(self, setting,text=None, image=None):
-        setting_types = ["text","image"]
-        if setting not in setting_types:
-            raise ValueError("Invalid data type. Expected one of: %s" % setting_types)
+        self.text_encoder = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
+        self.image_encoder = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch32")
+        self.linear1 = nn.Linear(1024,300)
+        self.linear2 = nn.Linear(300,2)
+        self.softmax = nn.Softmax(dim = 1)
 
-        if setting == "text":
-            text_outputs = self.text_encoder(text)
-            text_emd1 = text_outputs.last_hidden_state
-            text_emd2 = text_outputs.pooler_output
-            return text_emd1,text_emd2
+    def forward(self, text, image):
+        text_outputs = self.text_encoder(text).text_embeds
+        image_outputs = self.image_encoder(image).image_embeds
+        concat = torch.cat((text_outputs,image_outputs), 1)
+        out1 = self.linear1(concat)
+        out2 = self.linear2(out1)
+        prediction = self.softmax(out2)
 
-        elif setting == "image":
-            # encode image
-            image_outputs = self.image_encoder(image)
-            image_emd1 = image_outputs.last_hidden_state
-            image_emd2 = image_outputs.pooler_output
-            return image_emd1,image_emd2
+        return prediction
 
-
-class bla_model(nn.Module):
-    def __init__(self):
-        super(bla_model, self).__init__()
-        self.text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-base-patch32")
-        self.image_encoder = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
-    def forward(self, image):
-        image_outputs = self.image_encoder(image)
-        image_emd1 = image_outputs.last_hidden_state
-        image_emd2 = image_outputs.pooler_output
-        return image_emd1,image_emd2
-#model = CLIPVisionModel.from_pretrained("openai/clip-vit-base-patch32")
 
 model = clip_model()
-model2 = bla_model()
-
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-text_emds = list()
-for text in ["I have a dog","A vanilla ice cream"]:
-    # Tokenize the input text
-    input_ids = tokenizer.encode(text)
-    # Convert the input_ids to a tensor
-    input_tensor = torch.tensor([input_ids])
-    text_emd1, text_emd2 = model(setting="text",text = input_tensor)
-    text_emds.append(text_emd2)
-
-print(text_emds[0])
-
-processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-image_paths = ['/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1/trial_images_v1/image.86.jpg', '/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1/trial_images_v1/image.155.jpg']
-images = list()
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-transform = transforms.Compose(
-    [transforms.ToTensor(),transforms.Resize([1440, 1810]),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-     ])
-for path in image_paths:
-    image = Image.open(path)
-    if image.mode != "RGB":
-        image = image.convert('RGB')
-        print(type(image))
-    image = transform(image)
-    images.append(image)
-inputs = processor(images=images, return_tensors="pt")
-outputs = model2(image = inputs)
-last_hidden_state = outputs.last_hidden_state
-pooled_output = outputs.pooler_output  # pooled CLS states
-
-print(pooled_output.shape)
-"""
+for param in model.parameters():
+    print(param)
