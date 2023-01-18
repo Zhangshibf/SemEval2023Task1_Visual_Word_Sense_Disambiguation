c66703bf3999ada7298316bef34cbb6480bc23bc
zhangshibf <zhangshibf@gmail.com>
2023-01-18 22:27:45 +0100

    a

--- clip_model/train_linear.py
+++ clip_model/train_linear.py
@@ -88,11 +88,11 @@ def train_and_save_model(model,nn,device,epoch,path_train,path_out,optimizer):
         avg_loss = train_one_epoch(model,nn, device,train_dataloader, optimizer)
         print("--------------Loss per instance{}---------------".format(avg_loss))
         filepath = path_out+"/inferencemodel"+str(i)
-        torch.save(model.state_dict(), filepath)
+        torch.save(nn.state_dict(), filepath)
         print("--------------Model saved at {}---------------".format(filepath))
 
         state = {'epoch': epoch,
-            'state_dict': model.state_dict(),
+            'state_dict': nn.state_dict(),
             'optimizer': optimizer.state_dict()}
         filepath = path_out + "/trainingmodel" + str(i)
         torch.save(state, filepath)
