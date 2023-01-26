bc38a51d291fc217b60b1264afce128d192437f0
zhangshibf <zhangshibf@gmail.com>
2023-01-26 17:25:43 +0100

    a

--- clip_zeroshot/evaluation_clip_zero_shot.py
+++ clip_zeroshot/evaluation_clip_zero_shot.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     model = clip_model()
     model = model.to(device)
 
-    with open("/home/CE/zhangshi/SemEval23/clip_zeroshot/wordnet_wiki_dataset/dev.pk", 'rb') as pickle_file:
+    with open("/home/CE/zhangshi/SemEval23/clip_zeroshot/wiki_for_bio_dataset/dev.pk", 'rb') as pickle_file:
         dev_dataloader = pickle.load(pickle_file)
         pickle_file.close()
 
