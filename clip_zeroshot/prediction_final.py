e6e44f676cfd79eb4e4109477a44e654cd0bdfd0
zhangshibf <zhangshibf@gmail.com>
2023-01-27 15:56:52 +0100

    a

--- clip_zeroshot/prediction_final.py
+++ clip_zeroshot/prediction_final.py
@@ -83,7 +83,10 @@ def evaluate(model,device, dataloader,prediction_path):
             #write output
             indices = np.argsort(similarities)[::-1]
             sorted = np.take(image_names, indices)
+            sorted = sorted[0].reverse
             print(sorted)
+            string = sorted.join("\t")+"\n"
+            print(string)
 
     hit_rate = correct/total
     mrr = mrr/total
