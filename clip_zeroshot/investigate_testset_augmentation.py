1ea85bb817f7f2482a61f2e13b19934395ef1a73
zhangshibf <zhangshibf@gmail.com>
2023-01-28 13:27:04 +0100

    a

---
+++ clip_zeroshot/investigate_testset_augmentation.py
@@ -0,0 +1,18 @@
+#fine tune CLIP model
+from load_data_final_prediction import *
+from PIL import Image
+import argparse
+from PIL import ImageFile
+import torchvision.transforms as transforms
+from torch import nn
+from transformers import CLIPProcessor, CLIPVisionModelWithProjection,CLIPTokenizer, CLIPTextModelWithProjection
+import torch
+from torch import optim
+import clip
+
+with open("/home/CE/zhangshi/SemEval23/clip_zeroshot/testset_dataloader/dataset.pk", 'rb') as pickle_file:
+    dataloader = pickle.load(pickle_file)
+    pickle_file.close()
+
+for keywords,contexts,augmentations,image_names,image_paths in dataloader:
+    print(augmentations)
