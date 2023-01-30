3acef0a60418c3719cf73d9c1f2b79a12fb34143
zhangshibf <zhangshibf@gmail.com>
2023-01-30 16:45:02 +0100

    a

--- clip_zeroshot/investigate_trial.py
+++ clip_zeroshot/investigate_trial.py
@@ -1,6 +1,6 @@
 from load_data import *
 
-with open(/home/CE/zhangshi/dataloader_submission_trial/dataset.pk, 'rb') as pickle_file:
+with open("/home/CE/zhangshi/dataloader_submission_trial/dataset.pk", 'rb') as pickle_file:
     dataloader = pickle.load(pickle_file)
     pickle_file.close()
 
