8748bd4cb0f44258109210f12cd269b18ca13068
zhangshibf <zhangshibf@gmail.com>
2023-01-31 12:19:52 +0100

    a

---
+++ clip_zeroshot/load_data_prediction_it.py
@@ -0,0 +1,100 @@
+import os
+import pickle
+from torch.utils.data import Dataset, DataLoader,random_split
+import pandas as pd
+import argparse
+from nltk.corpus import wordnet as wn
+import nltk
+import italian_dictionary as dictionary
+from sentence_transformers import SentenceTransformer, util
+import numpy as np
+import wikipediaapi
+
+#this file is for italian
+class ImageTextDataset(Dataset):
+    def __init__(self, device,text_augmentation=True):
+        self.device = device
+        self.image_path = list()
+        self.image_name = list()
+
+        # this is for the original train set of the task
+        train_data = pd.read_csv("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/train_v1/mytestfile.txt", sep="\t", header=None)
+#        train_data = pd.read_csv("/home/CE/zhangshi/semeval_testset/en.test.data.v1.1.txt", sep="\t", header=None)
+#        train_data = pd.read_csv("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1/trial.data.v1.txt", sep="\t", header=None)
+
+        keywords = list(train_data[0])
+        contexts = list(train_data[1])
+        self.keywords = keywords
+        self.context = contexts
+
+        for i in range(len(train_data)):
+            self.image_name.append(list(train_data.loc[i, 2:]))
+
+        for row in self.image_name:
+            temporary = list()
+            for i in row:
+                temporary.append(os.path.join("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/train_v1/train_images_v1", i))
+                #temporary.append(os.path.join("/home/CE/zhangshi/sem/semeval-2023-task-1-V-WSD-train-v1/trial_v1/trial_images_v1", i))
+                #temporary.append(os.path.join("/home/CE/zhangshi/semeval_testset/test_images", i))
+
+            self.image_path.append(temporary)
+
+        #text augmentation
+        #an augmented text is composed of lemmas + definition from wordnet
+        if text_augmentation:
+            self.augmentation = list()
+            sent_encoder = SentenceTransformer('distiluse-base-multilingual-cased-v1').to(self.device)
+            for keyword,phrase in zip(self.keywords,self.context):
+                #retrieve all possible augmented texts
+                try:
+                    definitions = dictionary.get_definition(keyword)["definizione"]
+                    if len(definitions)>1:
+                        context_emb = sent_encoder.encode(phrase)
+                        aug_emb = sent_encoder.encode(definitions)
+                        scores = util.dot_score(context_emb, aug_emb)[0].tolist()
+                        idx = np.argmax(scores)
+                        self.augmentation.append(definitions[idx])
+                    elif len(definition)==1:
+                        self.augmentation.append(definitions[0])
+                except:
+                    self.augmentation.append(" ")
+
+
+    def __len__(self):
+        return len(self.context)
+
+    def __getitem__(self, idx):
+        # Load the image and text
+
+        #negative images
+        negative_images = list()
+        image_paths = self.image_path[idx]
+        image_names = self.image_name[idx]
+        paths = "#".join(image_paths)
+        names = "#".join(image_names)
+
+        context = self.context[idx]
+        keyword = self.keywords[idx]
+
+        positive_path = self.image_path[idx]
+        positive_name = self.image_name[idx]
+        if self.augmentation:
+            aug = self.augmentation[idx]
+
+        return keyword, context, aug,names, paths
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Build dataloader')
+    parser.add_argument("--cuda",help = "cuda number")
+    parser.add_argument("--output",help = "path to save the dataloader")
+    args = parser.parse_args()
+    # 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
+    device = 'cuda:'+str(args.cuda)
+    # Create the dataset
+    dataset = ImageTextDataset(device = device, text_augmentation=True)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    path = args.output+"/dataset.pk"
+
+    with open(path, 'wb') as f:
+        pickle.dump(dataloader, f)
---
+++ clip_zeroshot/prediction_it_final.py
@@ -0,0 +1,124 @@
+#fine tune CLIP model
+from load_data_prediction_it import *
+from PIL import Image
+import argparse
+from PIL import ImageFile
+import torchvision.transforms as transforms
+from torch import nn
+from transformers import CLIPProcessor, CLIPVisionModelWithProjection,CLIPTokenizer, CLIPTextModelWithProjection
+import torch
+from transformers import VisionTextDualEncoderModel
+from transformers import AutoProcessor
+from torch import optim
+
+class clip_model(nn.Module):
+
+    def __init__(self):
+        super(clip_model, self).__init__()
+        self.model = VisionTextDualEncoderModel.from_pretrained("clip-italian/clip-italian")
+        self.processor = AutoProcessor.from_pretrained("clip-italian/clip-italian")
+
+    def forward(self, text, image,setting):
+        if setting == "text":
+            text_outputs = self.embed_texts(text)
+            return text_outputs
+
+        elif setting == "image":
+            image_outputs = self.embed_images(image)
+            return image_outputs
+
+    def embed_texts(self,texts):
+        inputs = self.processor(text=texts, padding="longest")
+        input_ids = torch.tensor(inputs["input_ids"])
+        attention_mask = torch.tensor(inputs["attention_mask"])
+
+        with torch.no_grad():
+            embeddings = self.model.get_text_features(
+                input_ids=input_ids, attention_mask=attention_mask
+            )
+        return embeddings
+
+    def embed_images(self,images):
+        inputs = self.processor(images=images)
+        pixel_values = torch.tensor(np.array(inputs["pixel_values"]))
+
+        with torch.no_grad():
+            embeddings = self.model.get_image_features(pixel_values=pixel_values)
+        return embeddings
+
+
+def evaluate(model,device, dataloader,prediction_path):
+    #use normalized dot product
+    model.eval()
+    for keywords,contexts,augmentations,image_names,image_paths in dataloader:
+        image_names = [i.split("#") for i in image_names]
+        texts = list()
+        for k,c,a in zip(keywords,contexts, augmentations):
+            context_augmented = c + " " + a
+            texts.append(context_augmented)
+
+        paths = [i.split("#") for i in image_paths]
+        for keyword,context,t,ps in zip(keywords,contexts,texts,paths):
+            t = t.to(device)
+            t_emds = model(t, None, setting="text")
+            images = open_images(ps)
+            i_emds = model(None, images, setting="image")
+            i_emds = torch.stack(i_emds).squeeze().to(device)
+            t_emds = t_emds / t_emds.norm(dim=1, keepdim=True)
+            i_emds = i_emds / i_emds.norm(dim=1, keepdim=True)
+            similarities = torch.matmul(t_emds, i_emds.transpose(0, 1))
+            similarities = similarities.cpu()
+            similarities = similarities.detach().numpy()
+            # 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
+
+            #write output
+            indices = np.argsort(similarities)[::-1]
+            sorted = np.take(image_names, indices)
+            sorted = np.flip(sorted)
+            string = "\t".join(sorted.tolist()[0])+"\n"
+
+            with open(prediction_path,"a") as file:
+                file.write(string)
+                file.close()
+
+def open_images(image_paths):
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    transform = transforms.Compose(
+        [transforms.Resize([1440, 1810]), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+         ])
+    images = list()
+    for path in image_paths:
+        image = Image.open(path)
+        if image.mode != "RGB":
+            image = image.convert('RGB')
+        image = transform(image)
+        images.append(image)
+
+    return images
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Build dataloader')
+    parser.add_argument('--dataset', help="dataset used to predict result")
+    #/home/CE/zhangshi/SemEval23/clip_zeroshot/testset_dataloader/dataset.pk
+    parser.add_argument('--device', help="cuda number")
+    parser.add_argument("--output",help = "path to save the prediction")
+    args = parser.parse_args()
+
+    device_str = "cuda:" + args.device
+    device = torch.device(device_str)
+
+    model = clip_model()
+    model = model.to(device)
+    prediction_path = args.output
+
+    dataset_path = args.dataset
+    #batch size of the data has to be one!!!
+    with open(dataset_path, 'rb') as pickle_file:
+        dataloader = pickle.load(pickle_file)
+        pickle_file.close()
+
+    print("--------------Evaluation---------------")
+    evaluate(model,device, dataloader,prediction_path)
+    print("--------------Evaluation Finished---------------")
