5976e46d33dfaa15b773810f1fd660109bebdab0
zhangshibf <zhangshibf@gmail.com>
2023-01-31 13:01:52 +0100

    a

--- clip_zeroshot/prediction_it_final.py
+++ clip_zeroshot/prediction_it_final.py
@@ -66,8 +66,6 @@ def evaluate(model,device, dataloader,prediction_path):
 #            i_emds = torch.stack(i_emds).squeeze().to(device)
             t_emds = t_emds / t_emds.norm(dim=1, keepdim=True)
             i_emds = i_emds / i_emds.norm(dim=1, keepdim=True)
-            print(t_emds.size())
-            print(i_emds.size())
             similarities = torch.matmul(t_emds, i_emds.transpose(0, 1))
             similarities = similarities.cpu()
             similarities = similarities.detach().numpy()
